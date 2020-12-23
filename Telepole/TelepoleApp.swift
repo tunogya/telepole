376af31f067a7a1490f81f3710287067beffb42e
tunogya <tunogya@qq.com>
2020-12-23 15:09:47 +0800

    Initial Commit

---
+++ Telepole/TelepoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelepoleApp.swift
+//  Telepole
+//
+//  Created by 丁涯 on 2020/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TelepoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
