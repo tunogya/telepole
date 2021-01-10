90a7b39f0e35b3212669ea03d3174c4bcaa5047e
tunogya <tunogya@qq.com>
2021-01-10 13:13:37 +0800

    去掉 生日

--- Telepole/User.swift
+++ Telepole/User.swift
@@ -11,24 +11,33 @@ import SwiftyJSON
 let HOSTNAME = "https://app.wakanda.vip"
 
 struct User: Identifiable {
-    var id: String
+    var id = UUID()
+    var code: String
     var name: String
     var username: String
     var description: String
-    var profile_image_url: URL
+    var profile_image_url: String
     var protected: Bool
     var verified: Bool
     var variety: String
-    var birthday: String
     var gender: String
-    var shortbirthday: String {
-        return String(birthday.prefix(7))
-    }
+   
+}
+
+struct UserCreateParameters: Encodable {
+    let name: String
+    let username: String
+    let description: String
+//    let profile_image: FILE
+    let protected: Bool
+    let verified: Bool
+    let variety: String
+    let gender: String
 }
 
 // 根据ID获取用户数据
 public class UserFetcherById {
-    var user = User(id: <#T##String#>, name: <#T##String#>, username: <#T##String#>, description: <#T##String#>, profile_image_url: <#T##URL#>, protected: <#T##Bool#>, verified: <#T##Bool#>, variety: <#T##String#>, birthday: <#T##String#>, gender: <#T##String#>)
+    var user = User(code: "", name: "", username: "", description: "", profile_image_url: "", protected: false, verified: false, variety: "", gender: "boy")
     
     init(id: String) {
         self.load(id: id)
@@ -42,21 +51,34 @@ public class UserFetcherById {
             case .success(let value):
                 let jsonData = JSON(value)["data"]
                 self.user =  User(
-                    id: jsonData["_id"].stringValue,
+                    code: jsonData["_id"].stringValue,
                     name: jsonData["description"].stringValue,
                     username: jsonData["name"].stringValue,
                     description: jsonData["variety"].stringValue,
-                    profile_image_url: URL(string: jsonData["profile_image_url"].stringValue)!,
+                    profile_image_url: jsonData["profile_image_url"].stringValue,
                     protected: jsonData["protected"].boolValue,
                     verified: jsonData["verified"].boolValue,
                     variety: jsonData["variety"].stringValue,
-                    birthday: jsonData["birthday"].stringValue,
                     gender: jsonData["gender"].stringValue
                 )
                 print(self.user)
             case .failure(let error):
                 print(error)
             }
+        }
+    }
+}
+
+// 注册用户
+public class UserCreate {
+    func create(data: UserCreateParameters) {
+        let url = URL(string: "\(HOSTNAME)/telepole/v1.0/User/")!
+        
+        AF.request(url,
+                   method: .post,
+                   parameters: data,
+                   encoder: JSONParameterEncoder.default).response { response in
+            debugPrint(response)
         }
     }
 }
