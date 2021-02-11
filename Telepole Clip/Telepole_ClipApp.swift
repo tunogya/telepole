7820025e52a35d74a18529c4f5400bf628485d03
tunogya <tunogya@qq.com>
2021-02-11 14:26:56 +0800

    整理资源

---
+++ Telepole Clip/Telepole_ClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Telepole_ClipApp.swift
+//  Telepole Clip
+//
+//  Created by 丁涯 on 2021/2/11.
+//
+
+import SwiftUI
+
+@main
+struct Telepole_ClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
