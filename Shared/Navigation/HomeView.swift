edaaa2ae2831a4eb81c56582be5436994b5feaa3
tunogya <tunogya@qq.com>
2021-02-15 11:06:30 +0800

    优化动画

--- Shared/Navigation/HomeView.swift
+++ Shared/Navigation/HomeView.swift
@@ -42,6 +42,7 @@ struct HomeView: View {
                     
                     ShowPetInfo(isPresent: $isFoldMap)
                 }
+                .animation(Animation.openMap)
             }
             
             VStack(alignment: .leading){
@@ -84,7 +85,6 @@ struct HomeView: View {
             }
         }
         .padding(.horizontal)
-        .animation(.spring(dampingFraction: 0.618))
     }
 }
 
---
+++ Shared/Styles/Animations.swift
@@ -0,0 +1,12 @@
+//
+//  Animations.swift
+//  Telepole
+//
+//  Created by 丁涯 on 2021/2/15.
+//
+
+import SwiftUI
+
+extension Animation {
+    static let openMap = Animation.spring(dampingFraction: 0.618)
+}
