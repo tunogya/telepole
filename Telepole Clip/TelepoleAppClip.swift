bf4dafade5ac1a21981c6b6cf59a7def222c7cfc
tunogya <tunogya@qq.com>
2021-02-17 13:00:14 +0800

    初步完成clip代码

---
+++ Telepole Clip/TelepoleAppClip.swift
@@ -0,0 +1,43 @@
+//
+//  TelepoleClip.swift
+//  Telepole Clip
+//
+//  Created by 丁涯 on 2021/2/16.
+//
+
+import SwiftUI
+import AppClip
+import CoreLocation
+
+@main
+struct TelepoleAppClip: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .onContinueUserActivity(NSUserActivityTypeBrowsingWeb, perform: handleUserActivity)
+        }
+    }
+    
+    func handleUserActivity(_ userActivity: NSUserActivity) {
+        guard
+            let incomingURL = userActivity.webpageURL,
+            let components = NSURLComponents(url: incomingURL, resolvingAgainstBaseURL: true),
+            let queryItems = components.queryItems
+        else {
+            return
+        }
+        
+        print(queryItems)
+        
+        guard
+            let latitudeValue = queryItems.first(where: { $0.name == "latitude" })?.value,
+            let longitudeValue = queryItems.first(where: { $0.name == "longitude" })?.value,
+            let latitude = Double(latitudeValue),
+            let longitude = Double(longitudeValue)
+        else {
+            return
+        }
+        
+        print(latitude, longitude)
+    }
+}
